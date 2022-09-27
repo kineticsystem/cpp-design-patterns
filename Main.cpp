ff0818fb0a59a44b21c3d30cb2f14e0404bb887e
Giovanni Remigi <g.remigi@kineticsystem.org>
2022-09-27 11:36:44 +0200

    Concepts.

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,8 @@
 #include "builder/StepBuilder2.h"
 #include "builder/StepBuilder3.h"
 #include "builder/StepBuilder4.h"
+#include "expected/Expected.h"
+#include "factory/Factory1.h"
 #include "injection/Injection1.h"
 #include "injection/Injection2.h"
 #include "injection/Injection3.h"
@@ -9,55 +11,48 @@
 #include "injection/Injection5.h"
 #include "injection/Injection6.h"
 #include "injection/Injection7.h"
-#include "factory/Factory1.h"
-#include "pimpl/Widget.h"
-#include "pimpl/Label.h"
+#include "injection/Injection8.h"
 #include "mvc/ModelViewController1.h"
 #include "mvc/ModelViewController2.h"
-#include "expected/Expected.h"
+#include "pimpl/Label.h"
+#include "pimpl/Widget.h"
 
 class Expected {
 public:
-    bool ok() {
-        return true;
-    }
+  bool ok() { return true; }
 };
 
-Expected funct1() {
-    return {};
-}
-
+Expected funct1() { return {}; }
 
 int main() {
 
+  if (auto result = funct1(); !result.ok()) {
+    return 1;
+  } else if (result = funct1(); !result.ok()) {
+  }
 
-    if (auto result = funct1(); !result.ok()) {
-        return 1;
-    } else if (result = funct1();  !result.ok()) {
+  stepbuilder1::Test::execute();
+  stepbuilder2::Test::execute();
+  stepbuilder3::Test::execute();
+  stepbuilder4::Test::execute();
 
-    }
+  Injection1::Test::execute();
+  Injection2::Test::execute();
+  Injection3::Test::execute();
+  Injection4::Test::execute();
+  Injection5::Test::execute();
+  Injection6::Test::execute();
+  Injection7::Test::execute();
+  Injection8::Test::execute();
 
-    stepbuilder1::Test::execute();
-    stepbuilder2::Test::execute();
-    stepbuilder3::Test::execute();
-    stepbuilder4::Test::execute();
+  Factory1::Test::execute();
 
-    Injection1::Test::execute();
-    Injection2::Test::execute();
-    Injection3::Test::execute();
-    Injection4::Test::execute();
-    Injection5::Test::execute();
-    Injection6::Test::execute();
-    Injection7::Test::execute();
+  DPointer::Test::execute();
 
-    Factory1::Test::execute();
+  ModelViewController1::Test::execute();
+  ModelViewController2::Test::execute();
 
-    DPointer::Test::execute();
+  expected::Test::execute();
 
-    ModelViewController1::Test::execute();
-    ModelViewController2::Test::execute();
-
-    expected::Test::execute();
-
-    return 0;
+  return 0;
 }
---
+++ injection/Injection8.h
@@ -0,0 +1,134 @@
+#ifndef INJECTION_8_H
+#define INJECTION_8_H
+
+#include <iostream>
+#include <sstream>
+
+// DEPENDECY INJECTION WITH TEMPLATES AND CONCEPTS
+//
+// This time all dependencies are given as template arguments.
+//
+// This approach has the benefit of setting the dependencies at compile time
+// without incurring extra runtime overhead due to virtual functions.
+// The disadvantage is that dependencies cannot be changed at runtime.
+//
+// We can enforce template argument types using static asserts or we can inject
+// totally unrelated classes as long as they have methods with the same
+// signature.
+// Injected classes do not necessarily need to implement the same interface.
+// This is particularly interesting in testing because we are not required
+// a base interface to mock.
+//
+// See https://www.youtube.com/watch?v=yVogS4NbL6U
+
+namespace Injection8 {
+
+using namespace std;
+
+class D {
+  virtual string str() const = 0;
+};
+
+// D1 and D2 extends D.
+
+class D1 : public D {
+public:
+  string str() const override { return "D1"; }
+};
+
+class D2 : public D {
+public:
+  string str() const override { return "D2"; }
+};
+
+// C1 and C2 are unrelated concrete classes having methods with the same
+// signature.
+
+class C1 {
+public:
+  string str() const { return "C1"; }
+};
+
+class C2 {
+public:
+  string str() const { return "C2"; }
+};
+
+// B1 and B2 are unrelated classes having methods with the same signature.
+
+template <class T>
+concept DBase = std::is_base_of<D, T>::value;
+
+template <DBase T> class B1 {
+  T d;
+
+public:
+  explicit B1(const T &d) : d{d} {}
+  string str() const {
+    stringstream ss;
+    ss << "B1(";
+    ss << d.str();
+    ss << ")";
+    return ss.str();
+  }
+};
+
+class B2 {
+public:
+  string str() const {
+    stringstream ss;
+    ss << "B2";
+    return ss.str();
+  }
+};
+
+template <class B, class C> class A {
+  B b;
+  C c;
+
+public:
+  explicit A(const B &b, C const &c) : b{b}, c{c} {}
+  string str() const {
+    stringstream ss;
+    ss << "A(";
+    ss << b.str();
+    ss << ",";
+    ss << c.str();
+    ss << ")";
+    return ss.str();
+  }
+};
+
+/* ////////////////////////////////////////////////////////////////////////////
+ * Examples.
+ */
+struct Test {
+  static void execute() {
+
+    cout << "TEMPLATE DEPENDECY INJECTION" << endl;
+
+    C1 c1;
+    C2 c2;
+    D1 d1;
+
+    B1 b1{d1};
+    B2 b2;
+
+    // Injecting using template.
+    A a1{b1, c1};
+    cout << a1.str() << endl;
+
+    // Injecting using template.
+    A a2{b2, c1};
+    cout << a2.str() << endl;
+
+    // Injecting using template.
+    A a3{b1, c2};
+    cout << a3.str() << endl;
+
+    cout << endl;
+  }
+};
+} // namespace Injection8
+
+#endif // INJECTION_8_H
